1d2d353603ee48b6996338fa8263e4827300e9c0
Kledenilson Freire <kvpfjp@gmail.com>
2025-01-14 22:12:28 -0300

    ajustes de environment

--- doidos-descentralizados-app/src/services/api.js
+++ doidos-descentralizados-app/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = process.env.REACT_APP_HOST_API;
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Altere para o endpoint correto da sua API.
+  baseURL: BASE_URL, // Altere para o endpoint correto da sua API.
 });
 
 export default api;
